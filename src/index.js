585a4f0c0da12a9d67219218e8455b440b0bc132
Angel Manuel Martinez Fernandez <angelmanuelmartinezfernandez@iMac-de-Angel.local>
2020-12-29 18:24:51 +0100

    Creación de aplicación de tareas con datos persistentes en LocalStorage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-import { saludar } from './js/componentes.js';
-import './styles.css';
+import "./styles.css";
+
+import { Todo, TodoList } from "./classes";
+import { crearTodoHtml } from "./js/componentes";
+
+// import {Todo} from './classes/todo.class.js';
+// import {TodoList} from './classes/todo-list.class.js';
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(crearTodoHtml);
 
 
+// todoList.todos.forEach(todo => crearTodoHtml(todo));
 
-const nombre = 'Angel';
+// console.log(todoList.todos);
 
-saludar( nombre );
+// const tarea = new Todo("Comprar un Flip Off Finger");
+
+// todoList.nuevoTodo(tarea);
+
+// crearTodoHtml(tarea);
+
+// localStorage.setItem("mi-key", "ABC123");
